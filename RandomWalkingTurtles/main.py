089b8944f46703ea5961e0b04ff005743db66907
chetanharjani <chetonix@gmail.com>
2024-09-03 20:00:32 +0530

    change code in random-turtle-walk about collision with the wall

--- RandomWalkingTurtles/main.py
+++ RandomWalkingTurtles/main.py
@@ -12,9 +12,21 @@ while(True):
     random_move = random.randint(5, 50)
     random_turn = random.randint(0, 360)
 
-    if (che.xcor() > 280 or che.ycor() > 280 or che.xcor() < -280 or che.ycor() < -280):
-        che.backward(100)
+    if che.xcor() > 280:
+        che.setheading(180)
+        che.forward(100)
 
+    if che.ycor() > 280:
+        che.setheading(270)
+        che.forward(100)
+
+    if che.xcor() < -280:
+        che.setheading(0)
+        che.forward(100)
+
+    if che.ycor() < -280:
+        che.setheading(90)
+        che.forward(100)
     
     che.forward(random_move)
     che.left(random_turn)
