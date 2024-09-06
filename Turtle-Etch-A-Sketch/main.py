5d53c85f006f1d527d3632c9e72c2f5e288ff6bd
chetanharjani <chetonix@gmail.com>
2024-09-06 18:27:23 +0530

    add penup and pendown when we press w key and release it

--- Turtle-Etch-A-Sketch/main.py
+++ Turtle-Etch-A-Sketch/main.py
@@ -21,12 +21,19 @@ def go_left():
     che.setheading(180)
     che.forward(50)
 
+def pen_up():
+    che.penup()
+
+def pen_down():
+    che.pendown()
+
 scrn.listen()
 
 scrn.onkey(go_up, "Up")
 scrn.onkey(go_down, "Down")
 scrn.onkey(go_left, "Left")
 scrn.onkey(go_right, "Right")
-
+scrn.onkeypress(pen_up, "w")
+scrn.onkeyrelease(pen_down, "w")
 
 scrn.exitonclick()
