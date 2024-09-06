9f6891b43dfe2034b623c1b057409c4d99cb951a
chetanharjani <chetonix@gmail.com>
2024-09-06 18:35:08 +0530

    add fib series code

---
+++ Fib-Series/main.py
@@ -0,0 +1,14 @@
+def fib(n):
+    a = 0
+    b = 1
+    print(a, end=" ")
+    print(b, end = " ")
+    for _ in range(n-2):
+        c = a + b
+        print(c, end=" ")
+        a = b
+        b = c
+    print()
+
+    
+fib(20)
